0c75d6edcc18a3e9eda84309799e9510dfd50079
LauraPenaloza <lpenaloza059@gmail.com>
2023-05-28 11:14:17 -0500

    Primera implementación

--- Maestro.java
+++ Maestro.java
@@ -26,13 +26,17 @@ public class Maestro {
      * }
      */
 
-    public void Cuenta(String usser2, String password2) {
+    public boolean Cuenta(String usser2, String password2) {
+    	return usser2.equals(Usuario) && password2.equals(password1);  
+    }
+
+    /*public void Cuenta(String usser2, String password2) {
         if (Usuario.equals(usser2) && password1.equals(password2)) {
             System.out.println("go");
         } else {
             System.out.println("3333");
         }
-    }
+    }*/
 
     /*
      * public void CuentasCreadas(){
